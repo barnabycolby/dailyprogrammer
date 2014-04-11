1843d3ad78eccc60138094a41c286233ea832265
Barnaby Colby <barneycolby@gmail.com>
2014-04-11 12:40:08 +0100

    Added calls to empty cube class

---
+++ 157-Intermediate-Puzzle Cube Simulator/src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include "Cube.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	// Print a title
+	cout << "Puzzle Cube Simulator" << endl;
+	cout << "--------------------------------" << endl;
+
+	// Initialise the cube
+	Cube cube();
+
+	// Apply each move in turn
+	for (int i = 1; i < argc; i++) {
+		cout << "Applying move: " << argv[i] << endl;
+//		cube.applyMove(argv[i]);
+	}
+
+	// Print the output
+//	std::cout << endl << cube.front() << endl;
+
+	// Return cleanly
+	return 0;
+}
