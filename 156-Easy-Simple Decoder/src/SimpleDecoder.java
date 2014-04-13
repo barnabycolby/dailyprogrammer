4232de7800b5b791f55edafa159734c1c06ea796
barneycolby <barnaby_colby@hotmail.com>
2014-04-13 23:47:29 +0100

    Prints usage instructions

---
+++ 156-Easy-Simple Decoder/src/SimpleDecoder.java
@@ -0,0 +1,9 @@
+public class SimpleDecoder {
+	public static void main(String args[]) {
+		// Check that we have the correct number of arguments
+		if (args.length != 2) {
+			System.out.println("Usage: SimpleDecoder inputFile outputFile");
+			return;
+		}
+	}
+}
