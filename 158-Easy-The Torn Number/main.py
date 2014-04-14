8066181f3de52b41670927ec26f00507489a8770
barneycolby <barnaby_colby@hotmail.com>
2014-04-14 19:26:08 +0100

    Only check numbers with four unique digits

--- 158-Easy-The Torn Number/main.py
+++ 158-Easy-The Torn Number/main.py
@@ -9,8 +9,25 @@ def isTornNumber(n):
 	# Calculate whether the number is torn
 	return ((firstHalf + secondHalf) ** 2) == n
 
+# Checks whether the four digits are all different
+def allDifferent(n):
+	# Get each digit
+	first = n % 10
+	second = (n // 10) % 10
+	third = (n // 100) % 10
+	fourth = n // 1000
+
+	# Check that the numbers are all different
+	if first != second and first != third and first != fourth:
+		if second != third and second != fourth:
+			if third != fourth:
+				return True
+	
+	# Fallthrough
+	return False
+
 # Test every possible 4 digit number
 for i in range(1000,10000):
-	if isTornNumber(i):
+	if isTornNumber(i) and allDifferent(i):
 		if i != 3025:
 			print "Found a torn number: {}".format(i)
