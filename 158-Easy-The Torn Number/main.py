fa92bdfcc3cad86a4f60c265ad622d0090b11293
barneycolby <barnaby_colby@hotmail.com>
2014-04-14 18:53:48 +0100

    Find all torn numbers

---
+++ 158-Easy-The Torn Number/main.py
@@ -0,0 +1,15 @@
+# Checks whether a number is a torn number
+def isTornNumber(n):
+	# Get the first half of the number
+	firstHalf = n // 100
+
+	# Get the second half of the number
+	secondHalf = n % 100
+
+	# Calculate whether the number is torn
+	return ((firstHalf + secondHalf) ** 2) == n
+
+# Test every possible 4 digit number
+for i in range(1000,10000):
+	if isTornNumber(i):
+		print "Found a torn number: {}".format(i)
