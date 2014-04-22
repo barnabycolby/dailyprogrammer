0d72c195b9055ef8827714deb476a486b3b5734c
barneycolby <barnaby_colby@hotmail.com>
2014-04-22 22:41:50 +0100

    Calculate height of the image

--- 158-Intermediate-The ASCII Architect/main.py
+++ 158-Intermediate-The ASCII Architect/main.py
@@ -1,3 +1,28 @@
 # Read the input file into a string
 with open("input.txt", "r") as inputFile:
 	inputString=inputFile.read().replace('\n','')
+
+# Calculate the height of the image
+height = 0
+i = 0
+while i < len(inputString):
+	c = inputString[i]
+	value = 0
+	# If character is a number, add it's value to the count
+	if c.isdigit():
+		value += int(c)
+		# Skip the current character in the loop because we've already dealt with it
+		i += 1
+		c = inputString[i]
+	
+	# Add the characters value to the count
+	# The ASCII value of a is 97
+	value += ord(c) - 96
+
+	# Check whether the value is greater than the current height value
+	if value > height:
+		# Set the new height
+		height = value
+
+	# Increment the counter
+	i += 1
