3af80ad7411eb40946340e33df6fbb0966e22253
Barnaby Colby <barnaby.colby@gmail.com>
2016-08-12 21:04:04 +0100

    Added full implementation of 275.

--- 275-Easy-Splurthian Chemistry 101/solution.js
+++ 275-Easy-Splurthian Chemistry 101/solution.js
@@ -2,7 +2,26 @@
 (function () {
     'use strict';
 
-    module.exports = function () {
+    module.exports = function (nameMixedCase, symbolMixedCase) {
+        var symbol, name, firstIndex, secondIndex;
+
+        // Symbols must be exactly two letters
+        if (symbolMixedCase.length !== 2) {
+            return false;
+        }
+
+        // Before retrieving the indexes of symbol components in the element name, we need to convert both strings to lowercase in order to
+        // achieve case insensitivity
+        name = nameMixedCase.toLowerCase();
+        symbol = symbolMixedCase.toLowerCase();
+        firstIndex = name.indexOf(symbol[0]);
+        secondIndex = name.indexOf(symbol[1], firstIndex + 1);
+
+        // Check that the symbol letters are found in the elements name
+        if (firstIndex === -1 || secondIndex === -1) {
+            return false;
+        }
+
         return true;
     };
 }());
