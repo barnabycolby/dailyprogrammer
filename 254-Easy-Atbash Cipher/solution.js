4c7b8c1d672ec09f3d203feff6bfbce8cda008ab
Barnaby Colby <barnaby.colby@gmail.com>
2016-08-15 19:30:18 +0100

    Added solution for 254.

--- 254-Easy-Atbash Cipher/solution.js
+++ 254-Easy-Atbash Cipher/solution.js
@@ -2,7 +2,26 @@
 (function () {
     'use strict';
 
-    module.exports = function () {
-        return "";
+    function reverse(input) {
+        return input.split("").reverse().join("");
+    }
+
+    module.exports = function (cipherText) {
+        var i, alphabet, plainText, characterIndex;
+
+        alphabet = "abcdefghijklmnopqrstuvwxyz";
+        plainText = "";
+
+        for (i = 0; i < cipherText.length; i += 1) {
+            characterIndex = alphabet.indexOf(cipherText[i]);
+
+            if (characterIndex === -1) {
+                plainText += cipherText[i];
+            } else {
+                plainText += reverse(alphabet)[characterIndex];
+            }
+        }
+
+        return plainText;
     };
 }());
